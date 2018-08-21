b12beac2b2c4ee24ed0b797b4f9055010ce3ed7b
Codecat <spansjh@gmail.com>
2018-08-21 15:00:28 +0200

    Added error message instead of crashing on exception during conversion

--- hw2a000ff/FormMain.cs
+++ hw2a000ff/FormMain.cs
@@ -19,9 +19,9 @@ namespace hw2a000ff
 			Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
 
 #if DEBUG
-			textSourcePath.Text = @"d:\Work\Crackshell\ARPGProj\trunk\ARPG\bin\assets\";
-			textLevelsXmlPath.Text = @"d:\Work\Crackshell\ARPGProj\trunk\ARPG\bin\campaign\levels.xml";
-			textOutputPath.Text = @"d:\Work\Crackshell\SSBD\trunk\bin\scenarios\hwport\";
+			textSourcePath.Text = @"g:\Steam\steamapps\common\Hammerwatch\assets\";
+			textLevelsXmlPath.Text = @"g:\Steam\steamapps\common\Hammerwatch\assets_campaign\campaign\levels.xml";
+			textOutputPath.Text = @"g:\Steam\steamapps\common\Hammerwatch\assets_hwr\";
 #endif
 		}
 
@@ -192,214 +192,223 @@ namespace hw2a000ff
 			var waiting = new FormWaiting();
 
 			var thread = new Thread(() => {
-				var tmStart = DateTime.Now;
 				int fileCount = 0;
+				var tmStart = DateTime.Now;
 
-				var files = Directory.GetFiles(Settings.SourcePath, "*.xml", SearchOption.AllDirectories);
+				try {
+					var files = Directory.GetFiles(Settings.SourcePath, "*.xml", SearchOption.AllDirectories);
 
-				foreach (var fnm in files) {
-					string filename = fnm.Substring(Settings.SourcePath.Length).Replace('\\', '/');
-					string dir = Path.GetDirectoryName(filename);
-
-					var xml = XmlFile.FromFile(fnm);
-					var root = xml.Root.Children[0];
-
-					if (Settings.ConvertActors && root.Name == "actor") {
-						// Actor units
-						string filenameUnit = Path.ChangeExtension(filename, "unit");
-						waiting.SetStatus("Actor: " + filenameUnit);
-						fileCount++;
-
-						Program.Prepare(dir, filenameUnit);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
-							UnitConverter.Convert(xml, writer, "actor", Path.GetFileNameWithoutExtension(filenameUnit), filenameUnit);
-						}
-
-					} else if (Settings.ConvertProjectiles && root.Name == "projectile") {
-						// Projectile units
-						string filenameUnit = Path.ChangeExtension(filename, "unit");
-						waiting.SetStatus("Projectile: " + filenameUnit);
-						fileCount++;
-
-						Program.Prepare(dir, filenameUnit);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
-							UnitConverter.Convert(xml, writer, "projectile", Path.GetFileNameWithoutExtension(filenameUnit), filenameUnit);
-						}
-
-					} else if (Settings.ConvertDoodads && root.Name == "doodad") {
-						// Doodad units
-						string filenameUnit = Path.ChangeExtension(filename, "unit");
-						waiting.SetStatus("Doodad: " + filenameUnit);
-						fileCount++;
-
-						Program.Prepare(dir, filenameUnit);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
-							UnitConverter.Convert(xml, writer, "doodad", Path.GetFileNameWithoutExtension(filenameUnit), filenameUnit);
-						}
-
-					} else if (Settings.ConvertTilesets && root.Name == "tileset") {
-						// Tilesets
-						string filenameTileset = Path.ChangeExtension(filename, "tileset");
-						waiting.SetStatus("Tilemap: " + filenameTileset);
-						fileCount++;
-
-						Program.Prepare(dir, filenameTileset);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameTileset))) {
-							TilesetConverter.Convert(xml, writer);
-						}
-
-					} else if (Settings.ConvertItems && root.Name == "item") {
-						// Items (actually just units with behaviors)
-						string filenameUnit = Path.ChangeExtension(filename, "unit");
-						waiting.SetStatus("Item: " + filenameUnit);
-						fileCount++;
-
-						Program.Prepare(dir, filenameUnit);
-
-						var slot = "";
-
-						var behavior = root.Attributes["behavior"];
-						switch (behavior) {
-							case "food":
-							case "money":
-							case "key":
-							case "collectable":
-							case "mana":
-							case "life":
-							case "potion":
-							case "present": // uhh
-							case "upgrade":
-								slot = "item";
-								break;
-
-							case "breakable":
-							case "door":
-							case "bomb":
-							case "checkpoint": //TODO: This needs a behavior
-								slot = "doodad";
-								break;
-
-							default:
-								Console.WriteLine("WARNING: Unknown behavior '{0}'", behavior);
-								continue;
-						}
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
-							UnitConverter.Convert(xml, writer, slot, "", filenameUnit);
-						}
-
-					} else if (Settings.ConvertStrings && filename.StartsWith("language\\") && root.Name == "dictionary") {
-						// String files (almost the same format, but we want to append our language prefix)
-						string filenameLang = Path.ChangeExtension(filename, "lang");
-						waiting.SetStatus("Strings: " + filenameLang);
-						fileCount++;
-
-						Program.Prepare(dir, filenameLang);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameLang))) {
-							StringConverter.Convert(xml, writer);
-						}
-
-					} else if (Settings.ConvertSpeechStyles && root.Name == "speech") {
-						// Speech styles
-						string filenameUnit = Path.ChangeExtension(filename, "sval");
-						waiting.SetStatus("Speech style: " + filenameUnit);
-						fileCount++;
-
-						Program.Prepare(dir, filenameUnit);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
-							SpeechStyleConverter.Convert(xml, writer);
-						}
-
-					} else if (Settings.ConvertFonts && root.Name == "font") {
-						// Fonts
-						string filenameLocal = Path.ChangeExtension(filename, "fnt");
-						waiting.SetStatus("Font: " + filenameLocal);
-						fileCount++;
-
-						Program.Prepare(dir, filenameLocal);
-
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameLocal))) {
-							BitmapFontConverter.Convert(xml, writer);
-						}
-
-					}
-				}
-
-				if (Settings.ConvertLoot) {
-					if (fileCount == 0) {
-						waiting.SetStatus("WARNING: Can't convert loot if no units are being converted.");
-					} else {
-						string[] slots = LootConverter.GetSlots();
-						foreach (var slot in slots) {
-							string filenameLocal = slot + ".sval";
-							waiting.SetStatus("Loot: " + filenameLocal);
-							fileCount++;
-
-							Program.Prepare("loot", filenameLocal);
-
-							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + "loot/" + filenameLocal))) {
-								LootConverter.Convert(slot, writer);
-							}
-						}
-					}
-				}
-
-				var levelsPath = Settings.LevelsPath;
-
-				if (Settings.ConvertLevels && levelsPath != "" && File.Exists(levelsPath)) {
-					levelsPath = Path.GetDirectoryName(levelsPath) + "\\";
-
-					//TODO: Properly convert this into a scenario xml
-					var levelsXml = XmlFile.FromFile(levelsPath + "levels.xml");
-					var levelsTags = levelsXml.Root.FindTagsByName("level");
-					foreach (var level in levelsTags) {
-						Program.LevelKeys.Add(level.Attributes["id"], Path.ChangeExtension(level.Attributes["res"], "lvl"));
-					}
-
-					var levels = Directory.GetFiles(levelsPath + "levels\\", "*.xml", SearchOption.AllDirectories);
-					foreach (var level in levels) {
-						string filenameLocal = Path.ChangeExtension(level.Substring(levelsPath.Length + "levels\\".Length), "lvl");
-						waiting.SetStatus("Level: " + filenameLocal);
-						fileCount++;
-
-						Program.Prepare("levels/" + Path.GetDirectoryName(filenameLocal), filenameLocal);
-
-						XmlFile xmlLevel = XmlFile.FromFile(level);
-						using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + "levels/" + filenameLocal))) {
-							LevelConverter.Convert(xmlLevel, writer, filenameLocal);
-						}
-					}
-				}
-
-				if (Settings.ConvertSounds) {
-					var filesSbnks = Directory.GetFiles(Settings.SourcePath + "/sound", "*.xml", SearchOption.AllDirectories);
-
-					foreach (var fnm in filesSbnks) {
-						string filename = fnm.Substring(Settings.SourcePath.Length);
+					foreach (var fnm in files) {
+						string filename = fnm.Substring(Settings.SourcePath.Length).Replace('\\', '/');
 						string dir = Path.GetDirectoryName(filename);
 
 						var xml = XmlFile.FromFile(fnm);
 						var root = xml.Root.Children[0];
 
-						if (root.Name == "soundbank") {
-							// Soundbanks (are the *exact* same format, but we're parsing it anyway, we want to copy the sound files!)
-							string filenameSbnk = Path.ChangeExtension(filename, "sbnk");
-							waiting.SetStatus("Soundbank: " + filenameSbnk);
+						if (Settings.ConvertActors && root.Name == "actor") {
+							// Actor units
+							string filenameUnit = Path.ChangeExtension(filename, "unit");
+							waiting.SetStatus("Actor: " + filenameUnit);
 							fileCount++;
 
-							Program.Prepare(dir, filenameSbnk);
+							Program.Prepare(dir, filenameUnit);
 
-							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameSbnk))) {
-								SoundbankConverter.Convert(xml, writer, Path.GetFileNameWithoutExtension(filenameSbnk));
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
+								UnitConverter.Convert(xml, writer, "actor", Path.GetFileNameWithoutExtension(filenameUnit), filenameUnit);
+							}
+
+						} else if (Settings.ConvertProjectiles && root.Name == "projectile") {
+							// Projectile units
+							string filenameUnit = Path.ChangeExtension(filename, "unit");
+							waiting.SetStatus("Projectile: " + filenameUnit);
+							fileCount++;
+
+							Program.Prepare(dir, filenameUnit);
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
+								UnitConverter.Convert(xml, writer, "projectile", Path.GetFileNameWithoutExtension(filenameUnit), filenameUnit);
+							}
+
+						} else if (Settings.ConvertDoodads && root.Name == "doodad") {
+							// Doodad units
+							string filenameUnit = Path.ChangeExtension(filename, "unit");
+							waiting.SetStatus("Doodad: " + filenameUnit);
+							fileCount++;
+
+							Program.Prepare(dir, filenameUnit);
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
+								UnitConverter.Convert(xml, writer, "doodad", Path.GetFileNameWithoutExtension(filenameUnit), filenameUnit);
+							}
+
+						} else if (Settings.ConvertTilesets && root.Name == "tileset") {
+							// Tilesets
+							string filenameTileset = Path.ChangeExtension(filename, "tileset");
+							waiting.SetStatus("Tilemap: " + filenameTileset);
+							fileCount++;
+
+							Program.Prepare(dir, filenameTileset);
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameTileset))) {
+								TilesetConverter.Convert(xml, writer);
+							}
+
+						} else if (Settings.ConvertItems && root.Name == "item") {
+							// Items (actually just units with behaviors)
+							string filenameUnit = Path.ChangeExtension(filename, "unit");
+							waiting.SetStatus("Item: " + filenameUnit);
+							fileCount++;
+
+							Program.Prepare(dir, filenameUnit);
+
+							var slot = "";
+
+							var behavior = root.Attributes["behavior"];
+							switch (behavior) {
+								case "food":
+								case "money":
+								case "key":
+								case "collectable":
+								case "mana":
+								case "life":
+								case "potion":
+								case "present": // uhh
+								case "upgrade":
+									slot = "item";
+									break;
+
+								case "breakable":
+								case "door":
+								case "bomb":
+								case "checkpoint": //TODO: This needs a behavior
+									slot = "doodad";
+									break;
+
+								default:
+									Console.WriteLine("WARNING: Unknown behavior '{0}'", behavior);
+									continue;
+							}
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
+								UnitConverter.Convert(xml, writer, slot, "", filenameUnit);
+							}
+
+						} else if (Settings.ConvertStrings && filename.StartsWith("language\\") && root.Name == "dictionary") {
+							// String files (almost the same format, but we want to append our language prefix)
+							string filenameLang = Path.ChangeExtension(filename, "lang");
+							waiting.SetStatus("Strings: " + filenameLang);
+							fileCount++;
+
+							Program.Prepare(dir, filenameLang);
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameLang))) {
+								StringConverter.Convert(xml, writer);
+							}
+
+						} else if (Settings.ConvertSpeechStyles && root.Name == "speech") {
+							// Speech styles
+							string filenameUnit = Path.ChangeExtension(filename, "sval");
+							waiting.SetStatus("Speech style: " + filenameUnit);
+							fileCount++;
+
+							Program.Prepare(dir, filenameUnit);
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameUnit))) {
+								SpeechStyleConverter.Convert(xml, writer);
+							}
+
+						} else if (Settings.ConvertFonts && root.Name == "font") {
+							// Fonts
+							string filenameLocal = Path.ChangeExtension(filename, "fnt");
+							waiting.SetStatus("Font: " + filenameLocal);
+							fileCount++;
+
+							Program.Prepare(dir, filenameLocal);
+
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameLocal))) {
+								BitmapFontConverter.Convert(xml, writer);
+							}
+
+						}
+					}
+
+					if (Settings.ConvertLoot) {
+						if (fileCount == 0) {
+							waiting.SetStatus("WARNING: Can't convert loot if no units are being converted.");
+						} else {
+							string[] slots = LootConverter.GetSlots();
+							foreach (var slot in slots) {
+								string filenameLocal = slot + ".sval";
+								waiting.SetStatus("Loot: " + filenameLocal);
+								fileCount++;
+
+								Program.Prepare("loot", filenameLocal);
+
+								using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + "loot/" + filenameLocal))) {
+									LootConverter.Convert(slot, writer);
+								}
 							}
 						}
 					}
+
+					var levelsPath = Settings.LevelsPath;
+
+					if (Settings.ConvertLevels && levelsPath != "" && File.Exists(levelsPath)) {
+						levelsPath = Path.GetDirectoryName(levelsPath) + "\\";
+
+						//TODO: Properly convert this into a scenario xml
+						var levelsXml = XmlFile.FromFile(levelsPath + "levels.xml");
+						var levelsTags = levelsXml.Root.FindTagsByName("level");
+						foreach (var level in levelsTags) {
+							Program.LevelKeys.Add(level.Attributes["id"], Path.ChangeExtension(level.Attributes["res"], "lvl"));
+						}
+
+						var levels = Directory.GetFiles(levelsPath + "levels\\", "*.xml", SearchOption.AllDirectories);
+						foreach (var level in levels) {
+							string filenameLocal = Path.ChangeExtension(level.Substring(levelsPath.Length + "levels\\".Length), "lvl");
+							waiting.SetStatus("Level: " + filenameLocal);
+							fileCount++;
+
+							Program.Prepare("levels/" + Path.GetDirectoryName(filenameLocal), filenameLocal);
+
+							XmlFile xmlLevel = XmlFile.FromFile(level);
+							using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + "levels/" + filenameLocal))) {
+								LevelConverter.Convert(xmlLevel, writer, filenameLocal);
+							}
+						}
+					}
+
+					if (Settings.ConvertSounds) {
+						var filesSbnks = Directory.GetFiles(Settings.SourcePath + "/sound", "*.xml", SearchOption.AllDirectories);
+
+						foreach (var fnm in filesSbnks) {
+							string filename = fnm.Substring(Settings.SourcePath.Length);
+							string dir = Path.GetDirectoryName(filename);
+
+							var xml = XmlFile.FromFile(fnm);
+							var root = xml.Root.Children[0];
+
+							if (root.Name == "soundbank") {
+								// Soundbanks (are the *exact* same format, but we're parsing it anyway, we want to copy the sound files!)
+								string filenameSbnk = Path.ChangeExtension(filename, "sbnk");
+								waiting.SetStatus("Soundbank: " + filenameSbnk);
+								fileCount++;
+
+								Program.Prepare(dir, filenameSbnk);
+
+								using (StreamWriter writer = new StreamWriter(File.Create(Settings.OutputPath + filenameSbnk))) {
+									SoundbankConverter.Convert(xml, writer, Path.GetFileNameWithoutExtension(filenameSbnk));
+								}
+							}
+						}
+					}
+				} catch (Exception ex) {
+					BeginInvoke(new Action(() => {
+						MessageBox.Show("Conversion has thrown an exception! Conversion will be stopped.\n\n" + ex.ToString(), "Converter", MessageBoxButtons.OK, MessageBoxIcon.Error);
+						Enabled = true;
+						waiting.Close();
+					}));
+					return;
 				}
 
 				Invoke(new Action(() => {
